843ed15617ef18d444e440fffa9ba96944dfd813
System Administrator <root@joyces-iphone-2.gateway.2wire.net>
2013-01-11 17:20:23 -0800

    Initial commit!
    Let's get started shall we?

---
+++ password.php
@@ -0,0 +1,10 @@
+<?php
+require 'config.php';
+#combine the words, and make my life more fun
+$combined = $random1.$random2.$random3.$random4;
+
+
+echo "Original Words: ".$random1.", ".$random2.", ".$random3.", ".$random4."\n\n";
+echo "Combined: ".$combined."\n\n";
+echo "Original Salt: ".$originalSalt."\n\n";
+?>
